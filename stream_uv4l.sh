d8583fccec178062725ecd527177d8f1bdd89783
ShubhamCpp <shubham.chopra2906@gmail.com>
2015-12-12 12:22:28 +0530

    Create stream_uv4l.sh

---
+++ stream_uv4l.sh
@@ -0,0 +1,1 @@
+sudo uv4l -nopreview --auto-video_nr --driver raspicam --encoding mjpeg --width 640 --height 480 --framerate 20 --server-option '--port=9090' --server-option '--max-queued-connections=30' --server-option '--max-streams=25' --server-option '--max-threads=29'
